6fa6e9a46c38a92234d12aa03b30f9a0b0db6f11
narmnarmz <narmnarmz@gmail.com>
2014-12-16 15:04:42 +0700

    Revert "Revert "No Complete""
    This reverts commit ddadec6d471669e4ca57bc51342d47b77fd3b99e.

--- app/views/index/event.blade.php
+++ app/views/index/event.blade.php
@@ -77,7 +77,8 @@
 													</div>
 												</div>
 												<div class="criuse-pic-info-price">
-													<p><span>By People Name</span></p>
+													<p><img src="{{ Profile::find($e->user_id)->picture }}" class="img-circle">
+														<span>By {{ Profile::find($e->user_id)->firstname }} {{ Profile::find($e->user_id)->lastname }}</span></p>
 												</div>
 												<div class="detail-pro">
 													Catagory : {{ $e->category }}
@@ -92,7 +93,7 @@
 											<ul>
 												@if (!Auth::check())
 												@elseif (Auth::id() === $e->user_id)
-													<li><a class="c-hotel" href="myevent/{{$e->event_id}}"><span> </span>EDIT</a></li>
+													<li><a class="c-hotel" href="myevent/{{$e->event_id}}"><span> </span>MANAGE</a></li>
 												@else
 													<li><a class="c-hotel" href="#" onclick="confirmJoin({{$e->event_id}})"><span> </span>JOIN</a></li>
 												@endif
@@ -102,16 +103,16 @@
 												<div class="clear"> </div>
 											</ul>
 										</div>
-										<div class="criuse-info-right">
+										<!-- <div class="criuse-info-right">
 											<ul>
 
 												<li><a class="btn" href="myevent/{{$e->event_id}}">Manage</a></li>
-												<!-- <li><a class="c-face" href="#"><span> </span> </a></li>
+												<li><a class="c-face" href="#"><span> </span> </a></li>
 												<li><a class="c-twit" href="#"><span> </span> </a></li>
 												<li><a class="c-tub" href="#"><span> </span> </a></li>
 												<li><a class="c-pin" href="#"><span> </span> </a></li>
 											</ul>
-										</div>
+										</div> -->
 										<div class="clear"> </div>
 									</div>
 								</div>
@@ -124,7 +125,7 @@
 								</script>
 								@endforeach
 
-							<!-- Social Media Part! -->
+							<!-- Social Media Part!
 
 							<!-- <div class="criuse-grid">
 								<div class="criuse-grid-head">
