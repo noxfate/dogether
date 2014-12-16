850552a42bb80ee40bb96f8635a5e12ec0915088
narmnarmz <narmnarmz@gmail.com>
2014-12-16 15:01:36 +0700

    No Complete

--- app/views/index/eventdetail.blade.php
+++ app/views/index/eventdetail.blade.php
@@ -51,13 +51,77 @@
 					</div>
 				</div>
 				<!----//End-find-place---->
+			</div>
+			<div class="criuse-main">
+				<div class="wrap">
+					<div class="criuse-head1">
+						<h3>MY EVENT Detail</h3>
+					</div>
+					<div class="criuse-grids">
+						<div class="criuse-grid">
+							<div class="criuse-grid-head">
+								<div class="criuse-img">
+									<div class="criuse-pic">
+										<img src="../images/glass.jpg" title="criuse-name" />
+									</div>
+									<div class="criuse-pic-info">
+											<div class="criuse-pic-info-top">
+												<div class="criuse-pic-info-top-weather">
+													<p>Start {{ $data->time_start }}<span> </span></p>
+													<p>End {{ $data->time_end }}<span> </span></p>
+												</div>
+												<div class="criuse-pic-info-top-place-name">
 
-	<h1>This is Event Detail</h1>
+													<h2><label>{{ $data->size }} People</label>
+														<span>{{ $data->name }}</span>
+													</h2>
+
+												</div>
+											</div>
+											<div class="criuse-pic-info-price">
+												<p><span>By {{ Profile::find($data->user_id)->firstname }} {{ Profile::find($data->user_id)->lastname }}</span></p>
+											</div>
+											<div class="detail-pro">
+												Catagory : {{ $data->category }}
+											</div>
+											<div class="detail-pro">
+												{{ $data->detail }}
+											</div>
+									</div>
+								</div>
+								<div class="criuse-info">
+									<div class="criuse-info-left">
+										<ul>
+											<li><a href="#">Location: {{$data->location}}</a></li>
+										</ul>
+									</div>
+									<div class="clear">
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!--Accept/Decline Person-->
+				<div class='row'>
+					<div class="col-md-4">.col-md-1</div>
+					<div class="col-md-2">.col-md-1</div>
+					<div class="col-md-4">.col-md-1</div>
+					<div class="col-md-2">.col-md-1</div>
+
+				</div>
+
+
+
+
+			</div>
+		</div>
+
 	@if ($data != null)
 		@if ($flag == 'join')
-
 			<!-- Data, Collaborator -->
 			<br>
+
+
 			Event name : {{ $data->name }}<br>
 			Size : {{ $data->size }}<br>
 			Event Start : {{ $data->time_start }}<br>
@@ -82,6 +146,9 @@
 
 			<!-- Data, Collaborator, Accept/Decline Button -->
 			<br>
+			!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+			{{ Profile::find($data->user_id)->firstname }}
+
 			Event name : {{ $data->name }}<br>
 			Size : {{ $data->size }}<br>
 			Event Start : {{ $data->time_start }}<br>
