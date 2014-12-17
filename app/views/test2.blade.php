8b701bb32e6662744f3f554ccff7ad36c0012522
narmnarmz <narmnarmz@gmail.com>
2014-12-17 23:02:12 +0700

    Re-Achievement no login View

---
+++ app/views/test2.blade.php
@@ -0,0 +1,41 @@
+@extends('index.master')
+@section('content')
+	<div class="destinations">
+		<div class="destination-head">
+			<div class="wrap">
+				<h3>RANKING</h3>
+			</div>
+			<!---End-destinatiuons---->
+			<!--Ranking-->
+			<div class="find-place dfind-place">
+				<div class="wrap">
+						<div class="row ranking">
+							<div class="col-md-4"><img class="imgh" src="assets/img/ranking/0.png" alt="FOREVER ALONE"></div>
+							<div class="col-md-8" style="padding-right: 100px;">
+								<p class="ranking-head"><img src="images/ach.png" alt=""></p>
+								<p>เข้าร่วมกิจกรรมและทำตาม achievement เพื่อระดับที่สูงขึ้นกว่าเดิมโดยคุณจะได้รับการบรรจุเป็นระดับ forever alone ซึ่งเป็นระดับต่ำสุดเมื่อทำการสมัครสมาชิกกับเรา ระดับจะสูงขึ้นเมื่อปลด achievementได้ตามที่กำหนดไว้ มาร่วมกันเพิ่มระดับและทำกิจกรรมร่วมกับทุกคนกันเถอะ</p>
+								<p>
+									<br>
+									<span> <img class="imgr" src="/assets/img/ranking/1.png" alt="OUT GOING"> &nbsp&nbsp</span>
+									<span> <img class="imgr" src="/assets/img/ranking/2.png" alt="RISING STAR"> &nbsp&nbsp</span>
+									<span> <img class="imgr" src="/assets/img/ranking/3.png" alt="HOTTIE"> &nbsp&nbsp</span>
+									<span> <img class="imgr" src="/assets/img/ranking/4.png" alt="SOCIALIZATiON"> </span>	
+								</p>
+							</div>
+						</div>
+
+					<div class="clear"> </div>
+				</div>
+			</div>
+			<!----//End-ranking---->
+		</div>
+
+		<!-- achievement -->
+		<!-- end achievement -->
+	</div>
+
+@stop
+
+
+
+
