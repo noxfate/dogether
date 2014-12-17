e2fcadc287709e42ce31af8ed95108f17b138e51
narmnarmz <narmnarmz@gmail.com>
2014-12-18 03:40:07 +0700

    achievement no login edit

--- app/views/index/achvment.blade.php
+++ app/views/index/achvment.blade.php
@@ -12,10 +12,8 @@
 						<div class="row ranking">
 							<div class="col-md-4"><img class="imgh" src="assets/img/ranking/0.png" alt="FOREVER ALONE"></div>
 							<div class="col-md-8" style="padding-right: 100px;">
-								<p class="ranking-head"><img src="images/ach.png" alt=""></p>
-								<p>เข้าร่วมกิจกรรมและทำตาม achievement เพื่อระดับที่สูงขึ้นกว่าเดิม โดยคุณจะได้รับการบรรจุเป็นระดับ forever alone ซึ่งเป็นระดับต่ำสุดเมื่อคุณทำการสมัครสมาชิกกับเรา ระดับของคุณจะสูงขึ้นเมื่อปลดล็อค achievement ได้ตามที่กำหนดไว้ มาร่วมกันเพิ่มระดับและทำกิจกรรมร่วมกับทุกคนกันเถอะ</p>
+								<p class="ranking-head"><img src="images/achievement.png" alt=""></p>
 								<p>
-									<br>
 									<span> <img class="imgr" src="/assets/img/ranking/1.png" alt="OUT GOING"> &nbsp&nbsp</span>
 									<span> <img class="imgr" src="/assets/img/ranking/2.png" alt="RISING STAR"> &nbsp&nbsp</span>
 									<span> <img class="imgr" src="/assets/img/ranking/3.png" alt="HOTTIE"> &nbsp&nbsp</span>
