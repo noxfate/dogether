773c7df4f1e0498b95b8b4f3e8dc99d30a93fa07
narmnarmz <narmnarmz@gmail.com>
2014-12-18 03:29:34 +0700

    Slide Index

--- app/views/index/slide.blade.php
+++ app/views/index/slide.blade.php
@@ -30,25 +30,55 @@
 		                 <!-- /Texts container -->
 		            </div>
 		            <!-- /Duplicate to create more slides -->
-		            <div class="slide">
-		                <img src="images/slider-bg.jpg" alt=""/>
+		            <div class="slide"> 
+		                <!-- Slide image -->
+		                    <img src="images/slider-bg.jpg" alt=""/>
+		                <!-- /Slide image -->
+		                <!-- Texts container -->
 		                <div class="slide_content">
-		                     <div class="slide_content_wrap">
+		                    <div class="slide_content_wrap">
 		                        <!-- Text title -->
-		                        <h4 class="title">To travel is to live</h4>
+		                        <h4 class="title">Hot Promotion</h4>
 		                        <!-- /Text title -->
 		                        <!-- Text description -->
-		                        <p class="description">You Don't Need Magic to Disappear. All you need is a destination.</p>
+		                        <img class="resize" src="images/slide2.png">
+		                        <!-- <p class="description">You Don't Need Magic to Disappear. All you need is a destination.</p> -->
 		                        <!-- /Text description -->
 		                        <div class="slide-btns description">
 		                        	<ul>
-		                        		<li><a class="mapbtn" href="#">Show on the map </a></li>
+		                        		<!-- <li><a class="mapbtn" href="#">Show on the map </a></li> -->
 		                        		<li><a class="minfo" href="#">More info </a></li>
 		                        		<div class="clear"> </div>
 		                        	</ul>
 		                        </div>
 		                    </div>
 		                </div>
+		                 <!-- /Texts container -->
+		            </div>
+		            		            <div class="slide"> 
+		                <!-- Slide image -->
+		                    <img src="images/slider-bg.jpg" alt=""/>
+		                <!-- /Slide image -->
+		                <!-- Texts container -->
+		                <div class="slide_content">
+		                    <div class="slide_content_wrap">
+		                        <!-- Text title -->
+		                        <h4 class="title">Hot Promotion</h4>
+		                        <!-- /Text title -->
+		                        <!-- Text description -->
+		                        <img class="resize" src="images/slide3.png">
+		                        <!-- <p class="description">You Don't Need Magic to Disappear. All you need is a destination.</p> -->
+		                        <!-- /Text description -->
+		                        <div class="slide-btns description">
+		                        	<ul>
+		                        		<!-- <li><a class="mapbtn" href="#">Show on the map </a></li> -->
+		                        		<li><a class="minfo" href="#">More info </a></li>
+		                        		<div class="clear"> </div>
+		                        	</ul>
+		                        </div>
+		                    </div>
+		                </div>
+		                 <!-- /Texts container -->
 		            </div>
 		            <!--/slide -->
 		        </div>
